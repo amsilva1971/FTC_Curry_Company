cb41680dd719b3a4c6a05be0a29f5f2d8cb49bc3
Alexandre Marques da Silva <111449395+amsilva1971@users.noreply.github.com>
2024-11-08 10:00:22 -0300

    Update 1_visao_empresa.py

--- pages/1_visao_empresa.py
+++ pages/1_visao_empresa.py
@@ -6,7 +6,7 @@ from PIL import Image
 #import haversine
 #from haversine import haversine
 #import plotly.express as px
-import folium
+#import folium
 from streamlit_folium import folium_static
 
 st.set_page_config (page_title='Visão empresa', page_icon='', layout='wide') 
